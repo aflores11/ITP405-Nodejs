46227003b5fbafd044e59f697d7d9daf6b2d1114
Angel Flores <angelflo@usc.edu>
2021-04-19 16:38:55 -0700

    working homework 10

---
+++ express.js
@@ -0,0 +1,49 @@
+const { response } = require("express");
+const express = require("express");
+const axios = require("axios");
+const fs = require("fs");
+
+
+const app = express();
+
+app.get('/api/github/:username', (req, response) => {
+
+    const user = req.params.username;
+
+    fs.readFile(`${user}.txt`,"utf8", (error, data) => {
+        if(error){
+
+            const promise = axios.get(`https://api.github.com/users/${user}`, {
+                headers: {
+                    Accept: "application/json",
+                },
+            });
+
+            promise.then((githubResponse) => {
+                const count = githubResponse.data.public_repos;
+
+                fs.writeFile(`${user}.txt`, count, (error)=>{
+                    if(error){
+                        console.error(error);
+                    }
+                    else console.log(`Saved file for ${user}`);
+                });
+
+                response.json({
+                    RepoCount: count,
+                });
+            });
+        }
+        else{
+            response.json({
+                RepoCout: Number(data),
+            });
+
+        }
+    })
+
+    
+
+});
+
+app.listen(8000);
